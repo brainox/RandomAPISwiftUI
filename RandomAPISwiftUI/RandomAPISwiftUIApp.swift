67fa0c6e3313d7a93cf225c3e8883e909fa0e831
Obinna <obinna.aguwa@gmail.com>
2022-10-01 00:55:06 +0100

    Initial Commit

---
+++ RandomAPISwiftUI/RandomAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAPISwiftUIApp.swift
+//  RandomAPISwiftUI
+//
+//  Created by Obinna Aguwa on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
